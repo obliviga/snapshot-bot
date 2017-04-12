86c6c46a872e3bfd9e96e32e6f30a2a52c13fbc8
Anish Kshatriya <Anish.Kshatriya@infor.com>
2017-04-12 13:51:20 -0400

    removing unnecessary comments. indenting code correctly

--- index.js
+++ index.js
@@ -1,26 +1,20 @@
 /**
-* A Bot for Slack!
-*/
-
-
-/**
 * Define a function for initiating a conversation on installation
 * With custom integrations, we don't have a way to find out who installed us, so we can't message them :(
 */
 
 function onInstallation(bot, installer) {
   if (installer) {
-      bot.startPrivateConversation({user: installer}, function (err, convo) {
-          if (err) {
-              console.log(err);
-          } else {
-              convo.say('I am a bot that has just joined your team');
-              convo.say('You must now /invite me to a channel so that I can be of use!');
-          }
-      });
+    bot.startPrivateConversation({user: installer}, function (err, convo) {
+      if (err) {
+        console.log(err);
+      } else {
+        convo.say('I am a bot that has just joined your team');
+        convo.say('You must now /invite me to a channel so that I can be of use!');
+      }
+    });
   }
 }
-
 
 /**
 * Configure the persistence options
@@ -30,11 +24,11 @@ var config = {};
 if (process.env.MONGOLAB_URI) {
   var BotkitStorage = require('botkit-storage-mongo');
   config = {
-      storage: BotkitStorage({mongoUri: process.env.MONGOLAB_URI}),
+    storage: BotkitStorage({mongoUri: process.env.MONGOLAB_URI}),
   };
 } else {
   config = {
-      json_file_store: ((process.env.TOKEN)?'./db_slack_bot_ci/':'./db_slack_bot_a/'), //use a different name if an app or CI
+    json_file_store: ((process.env.TOKEN)?'./db_slack_bot_ci/':'./db_slack_bot_a/'), //use a different name if an app or CI
   };
 }
 
@@ -55,7 +49,6 @@ if (process.env.TOKEN || process.env.SLACK_TOKEN) {
   console.log('Error: If this is a custom integration, please specify TOKEN in the environment. If this is an app, please specify CLIENTID, CLIENTSECRET, and PORT in the environment');
   process.exit(1);
 }
-
 
 /**
 * A demonstration for how to handle websocket events. In this case, just log when we have and have not
@@ -131,53 +124,7 @@ controller.on('rtm_close', function (bot) {
   // you may want to attempt to re-open
 });
 
-
 /**
 * Core bot logic goes here!
 */
 // BEGIN EDITING HERE!
-
-controller.on('bot_channel_join', function (bot, message) {
-  bot.reply(message, "I'm here!")
-});
-
-controller.hears('hello', 'direct_message', function (bot, message) {
-  bot.reply(message, 'Hello!');
-});
-
-controller.hears('marco', 'direct_message', function(bot, message) {
-  // bot.api.users.list({}, function(err, list){
-  //   if (list && list.members) {
-  //     bot.reply(message, "polo");
-  //
-  //     var randomNumber = Math.floor(Math.random() * (list.members.length - 0 + 1)) + 0;
-  //
-  //     var randomName = list.members[randomNumber].real_name;
-  //
-  //     console.log(list.members[randomNumber].real_name);
-  //
-  //     if (randomName == null || randomName == "" || randomName == undefined) {
-  //       console.log('user has been deleted');
-  //     } else {
-  //       console.log('actual person');
-  //     }
-  //   }
-  // })
-});
-
-/**
-* AN example of what could be:
-* Any un-handled direct mention gets a reaction and a pat response!
-*/
-//controller.on('direct_message,mention,direct_mention', function (bot, message) {
-//    bot.api.reactions.add({
-//        timestamp: message.ts,
-//        channel: message.channel,
-//        name: 'robot_face',
-//    }, function (err) {
-//        if (err) {
-//            console.log(err)
-//        }
-//        bot.reply(message, 'I heard you loud and clear boss.');
-//    });
-//});
